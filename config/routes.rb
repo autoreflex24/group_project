ec4ce4a5a67ec586824aae7506b8c90116ef2c5c
Jordan Hudgens <jordan@devcamp.com>
2017-08-01 16:07:21 +0000

    Styling Weblinks

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'pages/home'
 
   get 'pages/feed'
 
@@ -7,4 +6,5 @@ Rails.application.routes.draw do
 
   resources :groups
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
 end
